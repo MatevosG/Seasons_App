90a6ebf468a3ff4f9fecfe792b66c327a5a84785
LENOVO <LENOVO@DESKTOP-V6FI0B2>
2021-10-04 15:24:42 +0400

    Add project files.

---
+++ Seasons_App/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Customer_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seasons seasons = new Seasons();
+
+            foreach (string item in seasons)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class Seasons : IEnumerable
+    {
+        private string[] _items = new string[] { "Winter", "Spring", "Summer", "Autumn" };      
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator(_items);
+        }
+    }
+    public class Enumerator : IEnumerator
+    {
+        private string[] items;
+        private int count;
+        public Enumerator(string[] items)
+        {
+            this.items = items;
+        }
+        public object Current => items[count++];
+        public bool MoveNext()
+        {
+            return count < items.Length;
+        }
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
